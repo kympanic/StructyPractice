999d3683358345ad318a2bdcf7ae6c3e2a19de69
Daniel Yoo <dacapo0509@gmail.com>
2024-01-31 15:40:29 -0800

    Solved 'compress' (.js)

---
+++ compress/compress.js
@@ -0,0 +1,24 @@
+const compress = (s) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while(j <= s.length){
+    if(s[j] === s[i]){
+      j+=1
+    }else{
+      let count = j - i;
+      if(count === 1){
+        result.push(s[i])
+      }else{
+             result.push(String(count),s[i])
+      }
+      i=j
+    }
+  }
+  return result.join('')
+};
+​
+module.exports = {
+  compress,
+};
+​
