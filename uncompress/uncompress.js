8f18035d4b3b0ad9ee7a34bfdfbaddeedc07a576
Daniel Yoo <dacapo0509@gmail.com>
2024-01-31 15:15:33 -0800

    Solved 'uncompress' (.js)

--- uncompress/uncompress.js
+++ uncompress/uncompress.js
@@ -1,23 +1,24 @@
-// TWO POINTERS
 const uncompress = (s) => {
+  let result = [];
   let numbers = "0123456789"
-  let result = []
-  let i = 0
+  let i = 0;
   let j = 0;
   while(j < s.length){
     if(numbers.includes(s[j])){
       j+=1;
     }else{
-        const num = Number(s.slice(i,j))
-        for(let count = 0; count < num; count += 1){
-          result.push(s[j])
-        }
-      j+=1;
+      const num = Number(s.slice(i,j))
+      for(let count = 0; count < num; count++){
+        result.push(s[j])
+      }
+      j+=1
       i=j
     }
   }
-    return result.join('')
   
+  
+  
+  return result.join('')
 };
 ​
 module.exports = {
