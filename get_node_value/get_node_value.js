da53e5e90e381b942198fd531a7238415c0d1435
Daniel Yoo <dacapo0509@gmail.com>
2024-02-06 15:01:25 -0800

    Solved 'get node value' (.js)

--- get_node_value/get_node_value.js
+++ get_node_value/get_node_value.js
@@ -6,14 +6,9 @@
 // }
 ​
 const getNodeValue = (head, index) => {
-  let current = head;
-  let count = 0;
-  while(current !== null){
-    if(count === index) return current.val;
-    current = current.next;
-    count += 1
-  }
-  return null
+  if(head === null) return null;
+  if(index === 0) return head.val
+  return getNodeValue(head.next,index-1)
 };
 ​
 module.exports = {
