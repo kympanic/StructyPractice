1cee8af00fc0302ce8aef016fc4884cb7769679d
Daniel Yoo <dacapo0509@gmail.com>
2024-02-08 19:51:31 -0800

    Solved 'remove node' (.js)

---
+++ remove_node/remove_node.js
@@ -0,0 +1,27 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+​
+const removeNode = (head, targetVal) => {
+  if(head.val === targetVal) return head.next;
+  let current = head;
+  let prev = null;
+  while(current !== null){
+    if(current.val === targetVal){
+      prev.next = current.next;
+      break;
+    } 
+    prev = current
+    current = current.next
+    
+  }
+  return head;
+};
+​
+module.exports = {
+  removeNode,
+};
+​
