8cb255f5d1bc0b4abf82417cbe11baef574a217b
Daniel Yoo <dacapo0509@gmail.com>
2024-02-15 11:55:50 -0800

    Solved 'zipper lists' (.js)

--- zipper_lists/zipper_lists.js
+++ zipper_lists/zipper_lists.js
@@ -1,4 +1,3 @@
-
 // class Node {
 //   constructor(val) {
 //     this.val = val;
@@ -7,28 +6,26 @@
 // }
 ​
 const zipperLists = (head1, head2) => {
-  
   let tail = head1;
   let current1 = head1.next;
   let current2 = head2;
   let count = 0;
-  
   while(current1 !== null && current2 !== null){
     if(count % 2 === 0){
       tail.next = current2;
-      current2 = current2.next
+      current2 = current2.next;
     }else{
       tail.next = current1;
       current1 = current1.next
     }
-    tail = tail.next
+   tail = tail.next
     count += 1;
-    
   }
-  if(current1 !== null) tail.next = current1;
-  if(current2 !== null)tail.next = current2
+​
+  if(current1 === null) tail.next = current2;
+  if(current2 === null) tail.next = current1;
   
-  return head1;
+  return head1
 };
 ​
 module.exports = {
