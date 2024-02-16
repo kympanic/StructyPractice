4d9e4f8be45b8c700f987f80ca88cf51cd3dd00b
Daniel Yoo <dacapo0509@gmail.com>
2024-02-16 14:52:23 -0800

    Solved 'intersection' (.js)

--- intersection/intersection.js
+++ intersection/intersection.js
@@ -1,15 +1,13 @@
 const intersection = (a, b) => {
-  let interArr = [];
-  let newSet = new Set(a);
-  console.log(newSet)
- for(let i = 0; i < b.length; i++){
-   if(newSet.has(b[i])){
-     interArr.push(b[i])
-   }
- } 
-  return interArr
+  let setA = new Set(a);
+  let result = []
+  for(let item of b){
+    if(setA.has(item)){
+      result.push(item)
+    }
+  }
+  return result
 };
-intersection([4,2,1,6], [3,6,9,2,10])
 ​
 module.exports = {
   intersection,
