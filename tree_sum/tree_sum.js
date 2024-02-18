fbb65e78f1310814ad257dc3d98b7d2e116d2979
Daniel Yoo <dacapo0509@gmail.com>
2024-02-17 16:42:43 -0800

    Solved 'tree sum' (.js)

---
+++ tree_sum/tree_sum.js
@@ -0,0 +1,25 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+​
+const treeSum = (root) => {
+  if(root === null) return 0;
+  let sum = 0;
+  let stack = [root]
+  while(stack.length){
+    let current = stack.pop();
+    sum += current.val
+    if(current.right !== null)stack.push(current.right)
+    if(current.left !== null)stack.push(current.left)
+  }
+  return sum
+};
+​
+module.exports = {
+  treeSum,
+};
+​
