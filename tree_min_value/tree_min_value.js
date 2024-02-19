1147fc943e56765459181307231675e0c2b17f90
Daniel Yoo <dacapo0509@gmail.com>
2024-02-19 14:06:59 -0800

    Solved 'tree min value' (.js)

--- tree_min_value/tree_min_value.js
+++ tree_min_value/tree_min_value.js
@@ -7,14 +7,11 @@
 // }
 ​
 const treeMinValue = (root) => {
-  if(root === null) return Infinity;
-  let smallestLeftValue = treeMinValue(root.left)
-  let smallestRightValue = treeMinValue(root.right)
-  return Math.min(root.val,smallestLeftValue,smallestRightValue)
+  if(root === null) return Infinity
+  return Math.min(root.val, treeMinValue(root.left), treeMinValue(root.right))
 };
 ​
 module.exports = {
   treeMinValue,
 };
 ​
-  const rightValues = depthFirstValues(root.right)
