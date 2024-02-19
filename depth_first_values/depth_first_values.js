400fdb61aa1b0273db73b8d1360f3a5e1a96985b
Daniel Yoo <dacapo0509@gmail.com>
2024-02-19 13:39:00 -0800

    Solved 'depth first values' (.js)

--- depth_first_values/depth_first_values.js
+++ depth_first_values/depth_first_values.js
@@ -7,21 +7,24 @@
 // }
 ​
 const depthFirstValues = (root) => {
-  if(root === null) return [];
-  let result = [];
-  let stack = [root]
-  while(stack.length > 0){
-    let node = stack.pop();
-    result.push(node.val);
+//   if(root === null) return [];
+//   let result = [];
+//   let stack = [root]
+//   while(stack.length > 0){
+//     let node = stack.pop();
+//     result.push(node.val);
     
-    if(node.right !== null) stack.push(node.right)
-    if(node.left !== null) stack.push(node.left)
-  }
-  return result
+//     if(node.right !== null) stack.push(node.right)
+//     if(node.left !== null) stack.push(node.left)
+//   }
+//   return result
+  if(root === null) return [];
+  const leftValues = depthFirstValues(root.left)
+  const rightValues = depthFirstValues(root.right)
+  return [root.val, ...leftValues,...rightValues]
 };
 ​
 module.exports = {
   depthFirstValues,
 };
 ​
-  }
