d022ea51d70c05927eb72a244fd5710764349560
Daniel Yoo <dacapo0509@gmail.com>
2024-02-22 14:32:12 -0800

    Solved 'linked list find' (.js)

--- linked_list_find/linked_list_find.js
+++ linked_list_find/linked_list_find.js
@@ -6,9 +6,12 @@
 // }
 ​
 const linkedListFind = (head, target) => {
-    if(head === null)return false;
-  if(head.val === target) return true;
-  return linkedListFind(head.next,target)
+  let current = head;
+  while(current !== null){
+    if(current.val === target) return true;
+    current = current.next;
+  }
+  return false
 };
 ​
 module.exports = {
