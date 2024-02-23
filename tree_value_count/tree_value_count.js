5165522c550a80c4f286a13c9b9df0b33e534fee
Daniel Yoo <dacapo0509@gmail.com>
2024-02-22 16:00:53 -0800

    Solved 'tree value count' (.js)

--- tree_value_count/tree_value_count.js
+++ tree_value_count/tree_value_count.js
@@ -7,9 +7,17 @@
 // }
 ​
 const treeValueCount = (root, target) => {
-  if(root === null) return 0;
-  const match = root.val === target ? 1 : 0;
-  return match + treeValueCount(root.left,target) + treeValueCount(root.right,target)
+  if(root === null) return 0
+  let count = 0;
+  let stack = [root]
+  while(stack.length > 0){
+    let node = stack.pop()
+    if(node.val === target) count += 1;
+    
+    if(node.right !== null) stack.push(node.right)
+    if(node.left !== null) stack.push(node.left)
+  }
+  return count
 };
 ​
 module.exports = {
