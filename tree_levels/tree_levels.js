d4c3b1c5fc6bd87bc2c0a715e3039ad5a2563175
Daniel Yoo <dacapo0509@gmail.com>
2024-02-26 09:49:27 -0800

    Solved 'tree levels' (.js)

--- tree_levels/tree_levels.js
+++ tree_levels/tree_levels.js
@@ -1,3 +1,4 @@
+
 // class Node {
 //   constructor(val) {
 //     this.val = val;
@@ -6,47 +7,25 @@
 //   }
 // }
 ​
-const treeLevels = (root)=> {
-  let levels = [];
-  fillLevels(root,levels,0)
-  
-  return levels;
-}
-​
-const fillLevels = (root,levels, levelNum)=> {
+const treeLevels = (root) => {
   if(root === null) return [];
-  
-  if(levels.length === levelNum){
-    levels.push([root.val])
-  }else{
-    levels[levelNum].push(root.val)
-  }
-  
-  
-  fillLevels(root.left,levels,levelNum + 1)
-  fillLevels(root.right,levels,levelNum + 1)
-  
-}
+  let levels = [];
+  let stack = [{node:root, levelNum: 0}]
+  while(stack.length > 0){
+    let {node, levelNum} = stack.pop();
+    
+    if(levels.length === levelNum){
+      levels.push([node.val])
+    }else{
+      levels[levelNum].push(node.val)
+    }
 ​
-// const treeLevels = (root) => {
-//   if(root === null) return [];
-//   let levels = [];
-//   let stack = [ { node:root, levelNum: 0} ];
-//   while(stack.length > 0){
-//     const { node, levelNum} = stack.pop();
+    if(node.right !== null) stack.push({node:node.right, levelNum: levelNum +1})
+    if(node.left !== null) stack.push({node:node.left, levelNum: levelNum + 1})
     
-//     if(levels.length === levelNum){
-//       levels.push([node.val])
-//     }else{
-//       levels[levelNum].push(node.val)
-//     }
-    
-//     if(node.right !== null) stack.push({node:node.right, levelNum: levelNum + 1})
-//     if(node.left !== null) stack.push({node:node.left, levelNum: levelNum + 1})
-    
-//   }
-//   return levels;
-// };
+  }
+  return levels;
+};
 ​
 module.exports = {
   treeLevels,
