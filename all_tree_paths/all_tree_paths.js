06ce95125de6e2b440291781bb282db79247bb21
Daniel Yoo <dacapo0509@gmail.com>
2024-02-25 18:03:35 -0800

    Solved 'all tree paths' (.js)

--- all_tree_paths/all_tree_paths.js
+++ all_tree_paths/all_tree_paths.js
@@ -1,45 +1,30 @@
-class Node {
-  constructor(val){
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-​
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-​
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-​
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 ​
 const allTreePaths = (root) => {
-  if(root === null) return [] ;
-  if(root.left === null && root.right === null) return [[root.val]];
+  if(root === null) return []
+  if(root.left === null && root.right == null) return [[root.val]]
   
-  let paths = [];
-  let leftPath = allTreePaths(root.left)
-  for(let path of leftPath){
-    console.log(path,"this is left")
+  let paths = []
+  
+  let lethPath = allTreePaths(root.left)
+  
+  for(let path of lethPath){
     paths.push([root.val,...path])
   }
   let rightPath = allTreePaths(root.right)
   for(let path of rightPath){
-    console.log(path,"this is right")
     paths.push([root.val,...path])
   }
   
-  return paths
+  
+  return paths;
 };
-​
-allTreePaths(a)
 ​
 module.exports = {
   allTreePaths,
