aedd5a27a60f17349069c412d93635a0bafa3246
Daniel Yoo <dacapo0509@gmail.com>
2024-02-26 09:53:14 -0800

    Solved 'tree levels' (.js)

--- tree_levels/tree_levels.js
+++ tree_levels/tree_levels.js
@@ -8,24 +8,21 @@
 // }
 ​
 const treeLevels = (root) => {
-  if(root === null) return [];
   let levels = [];
-  let stack = [{node:root, levelNum: 0}]
-  while(stack.length > 0){
-    let {node, levelNum} = stack.pop();
-    
-    if(levels.length === levelNum){
-      levels.push([node.val])
-    }else{
-      levels[levelNum].push(node.val)
-    }
-​
-    if(node.right !== null) stack.push({node:node.right, levelNum: levelNum +1})
-    if(node.left !== null) stack.push({node:node.left, levelNum: levelNum + 1})
-    
-  }
+  fillLevels(root,levels,0)
   return levels;
 };
+​
+const fillLevels = (root,levels,levelNum) => {
+  if(root === null) return;
+  if(levels.length === levelNum){
+    levels.push([root.val])
+  }else{
+    levels[levelNum].push(root.val)
+  }
+  fillLevels(root.left, levels, levelNum + 1)
+  fillLevels(root.right, levels, levelNum + 1)
+}
 ​
 module.exports = {
   treeLevels,
