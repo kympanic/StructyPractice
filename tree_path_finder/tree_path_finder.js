5b726d14547a4b294c6eeefe73211143f6e30f71
Daniel Yoo <dacapo0509@gmail.com>
2024-02-26 09:26:58 -0800

    Solved 'tree path finder' (.js)

--- tree_path_finder/tree_path_finder.js
+++ tree_path_finder/tree_path_finder.js
@@ -10,11 +10,14 @@ const pathFinder = (root, target) => {
   if(root === null) return null;
   if(root.val === target) return [root.val]
   
-  let leftSide = pathFinder(root.left,target)
-  if(leftSide !== null) return [root.val,...leftSide]
-  
-  let rightSide = pathFinder(root.right,target)
-  if(rightSide !== null) return [root.val,...rightSide]
+  let leftPath = pathFinder(root.left,target)
+  if(leftPath !== null){
+    return [root.val,...leftPath]
+  }
+  let rightPath = pathFinder(root.right,target)
+  if(rightPath !== null){
+    return [root.val,...rightPath]
+  }
   
   return null
 };
