28636967ae637f7e3e0c879f8a0bc074ac8c3454
Daniel Yoo <dacapo0509@gmail.com>
2024-02-26 14:27:20 -0800

    Solved 'intersection' (.js)

--- intersection/intersection.js
+++ intersection/intersection.js
@@ -1,12 +1,13 @@
 const intersection = (a, b) => {
-  let setA = new Set(a);
   let results = []
+  let setA = new Set(a)
   for(let char of b){
     if(setA.has(char)){
       results.push(char)
     }
   }
-return results;
+​
+  return results;
 };
 ​
 module.exports = {
