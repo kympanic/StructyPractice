80a614d56b900b2a9bf61e8b4c451be814f4dbe4
Daniel Yoo <dacapo0509@gmail.com>
2024-02-27 23:08:45 -0800

    Solved 'remove node' (.js)

--- remove_node/remove_node.js
+++ remove_node/remove_node.js
@@ -6,17 +6,21 @@
 // }
 ​
 const removeNode = (head, targetVal) => {
-  if(head.val === targetVal) return head.next
+  if(head.val === targetVal) return head.next;
+  
   let current = head;
   let prev = null;
   while(current !== null){
+    let nextNode = current.next;
     if(current.val === targetVal){
-      prev.next = current.next;
+      prev.next = nextNode;
       break;
     }
     prev = current;
-    current = current.next
+    current = current.next;
   }
+  
+  
   return head;
 };
 ​
@@ -24,3 +28,4 @@ module.exports = {
   removeNode,
 };
 ​
+      currentStreak +=1
