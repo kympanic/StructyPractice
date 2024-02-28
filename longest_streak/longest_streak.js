1eefec8113c6dffbaa28257e62603382fb108137
Daniel Yoo <dacapo0509@gmail.com>
2024-02-27 23:00:55 -0800

    Solved 'longest streak' (.js)

--- longest_streak/longest_streak.js
+++ longest_streak/longest_streak.js
@@ -9,21 +9,22 @@ const longestStreak = (head) => {
   let maxStreak = 0;
   let currentStreak = 0;
   let current = head;
-  let prev = null;
+  let previousValue = null;
   while(current !== null){
-    if(current.val === prev){
-      currentStreak += 1
+    if(current.val === previousValue){
+      currentStreak +=1
     }else{
       currentStreak = 1;
     }
-    prev = current.val;
+    previousValue = current.val
     current = current.next;
     
     if(currentStreak > maxStreak){
       maxStreak = currentStreak;
+    }
   }
-  }
-  return maxStreak
+  
+  return maxStreak;
 };
 ​
 module.exports = {
