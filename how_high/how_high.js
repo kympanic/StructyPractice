4d798b20183806c898535d1a0a61d81f3f5cf91f
Daniel Yoo <dacapo0509@gmail.com>
2024-02-27 23:27:11 -0800

    Solved 'how high' (.js)

--- how_high/how_high.js
+++ how_high/how_high.js
@@ -8,13 +8,12 @@
 ​
 const howHigh = (node) => {
   if(node === null) return -1;
-  let leftSide = howHigh(node.left)
-  let rightSide = howHigh(node.right)
-  console.log(leftSide)
-  return 1 + Math.max(leftSide,rightSide)
+  if(node.left === null && node.right === null) return 0;
+  return 1 + Math.max(howHigh(node.left), howHigh(node.right))
 };
 ​
 module.exports = {
   howHigh,
 };
 ​
+  
