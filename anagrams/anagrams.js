f54386f221d9592f0c776f4d2ca55a0b967816be
Daniel Yoo <dacapo0509@gmail.com>
2024-03-23 16:20:53 -0700

    Solved 'anagrams' (.js)

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -1,28 +1,30 @@
 const anagrams = (s1, s2) => {
-  if(s1.length !== s2.length)return false;
-  
   let counter = {}
+  if(s1.length !== s2.length) return false;
   
   for(let char of s1){
     if(!(char in counter)){
-      counter[char] = 0;
+      counter[char] = 1;
+    }else{
+      counter[char] += 1;
     }
-    counter[char] += 1;
   }
   
   for(let char of s2){
-    if(counter[char] === undefined){
+    if(char in counter){
+      counter[char] -= 1
+    }else{
       return false;
     }
-    counter[char] -= 1
   }
   
-  for(let char in counter){
-    if(counter[char] !== 0) return false;
+  for(char in counter){
+    if(counter[char] !== 0){
+      return false;
+    }
   }
   
-  return true;
-​
+  return true
 };
 ​
 module.exports = {
