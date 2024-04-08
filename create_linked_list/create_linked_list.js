bbe68181cf7aabde9dd3e118e6efd21f4b62d712
Daniel Yoo <dacapo0509@gmail.com>
2024-04-08 15:49:34 -0700

    Solved 'create linked list' (.js)

--- create_linked_list/create_linked_list.js
+++ create_linked_list/create_linked_list.js
@@ -1,23 +1,25 @@
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
-​
+
+
 const createLinkedList = (values) => {
-  let dummyHead = new Node(null)
-  let tail = dummyHead;
-  
-  for(let char of values){
-    tail.next = new Node(char)
-    tail = tail.next;
+  let dummyHead = new Node(null)
+  let tail = dummyHead;
+  for(let char of values){
+    tail.next = new Node(char)
+    tail = tail.next;
   }
-  return dummyHead.next;
+
+
+  return dummyHead.next
 };
-​
+
+
 module.exports = {
-  createLinkedList,
+  createLinkedList,
 };
-​
-      newNode.next = current.next.next;
+
