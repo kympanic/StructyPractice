7df353e1ddabd5ce59143ea7496ccb9e9f998ccf
Daniel Yoo <dacapo0509@gmail.com>
2024-04-08 15:54:43 -0700

    Solved 'breadth first values' (.js)

--- breadth_first_values/breadth_first_values.js
+++ breadth_first_values/breadth_first_values.js
@@ -1,26 +1,31 @@
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
 // }
-​
+
+
 const breadthFirstValues = (root) => {
-  if(root === null) return [];
-  let queue = [root]
-  let results = [];
-  while(queue.length){
-    let node = queue.shift();
-    results.push(node.val)
-    if(node.left !== null) queue.push(node.left)
-    if(node.right !== null) queue.push(node.right)
+  if(root === null) return [];
+  let queue = [root]
+  let results = []
+  while(queue.length > 0){
+    let node = queue.shift()
+    results.push(node.val)
+
+
+    if(node.left !== null) queue.push(node.left)
+    if(node.right !== null) queue.push(node.right)
   }
-  
-  return results;
+
+
+  return results;
 };
-​
+
+
 module.exports = {
-  breadthFirstValues,
+  breadthFirstValues,
 };
-​
+
