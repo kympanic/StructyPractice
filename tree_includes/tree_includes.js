c69cf90183ebbe678cfc102265f8dc91e1f3da9a
Daniel Yoo <dacapo0509@gmail.com>
2024-04-08 15:57:21 -0700

    Solved 'tree includes' (.js)

--- tree_includes/tree_includes.js
+++ tree_includes/tree_includes.js
@@ -1,18 +1,22 @@
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
 // }
-​
+
+
 const treeIncludes = (root, target) => {
-  if(root === null) return false;
-  if(root.val === target) return true;
-  return treeIncludes(root.right,target) || treeIncludes(root.left,target)
+  if(root === null) return false;
+  if(root.val === target) return true;
+
+
+  return treeIncludes(root.left,target) || treeIncludes(root.right,target)
 };
-​
+
+
 module.exports = {
-  treeIncludes,
+  treeIncludes,
 };
-​
+
