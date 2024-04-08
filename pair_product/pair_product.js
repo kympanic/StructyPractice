46115ea48c5ddaf0502adebc3d1d7a00af02ca9c
Daniel Yoo <dacapo0509@gmail.com>
2024-04-08 15:09:09 -0700

    Solved 'pair product' (.js)

--- pair_product/pair_product.js
+++ pair_product/pair_product.js
@@ -1,16 +1,23 @@
 const pairProduct = (numbers, targetProduct) => {
-  let counter = {};
-  for(let i = 0; i < numbers.length;i++){
-    let num = numbers[i]
-    let complement = targetProduct / num;
-    if(complement in counter) return [i,counter[complement]]
-    
-    counter[num] = i;
+  let counter = {};
+
+
+  for(let i = 0; i < numbers.length; i++){
+    let num = numbers[i]
+    let complement = targetProduct / num;
+    if(complement in counter){
+      return [counter[complement], i]
+    }
+    
+    counter[num] = i; 
   }
+
+
+  
 };
-​
+
+
 module.exports = {
-  pairProduct,
+  pairProduct,
 };
-​
-      return false;
+
