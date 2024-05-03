dfaf45e54023936f267b0455f10c7d881446f619
Daniel Yoo <dacapo0509@gmail.com>
2024-05-03 14:24:52 -0700

    Solved 'anagrams' (.js)

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -1,33 +1,44 @@
+anagrams('restful', 'fluster'); // -> true
+anagrams('cats', 'tocs'); // -> false
+anagrams('monkeyswrite', 'newyorktimes'); // -> true
+anagrams('paper', 'reapa'); // -> false
+anagrams('elbow', 'below'); // -> true
+anagrams('tax', 'taxi'); // -> false
+anagrams('taxi', 'tax'); // -> false
+anagrams('night', 'thing'); // -> true
+anagrams('abbc', 'aabc'); // -> false
+anagrams('po', 'popp'); // -> false
+anagrams('pp', 'oo') // -> false
 const anagrams = (s1, s2) => {
-  let counter = {}
-  if(s1.length !== s2.length) return false;
-  
-  for(let char of s1){
-    if(!(char in counter)){
-      counter[char] = 1;
-    }else{
-      counter[char] += 1;
-    }
+  if(s1.length !== s2.length) return false;
+  let counter ={};
+  for(let char of s1){
+    if(!(char in counter)){
+      counter[char] = 0;
+    }
+    counter[char] +=1
   }
-  
-  for(let char of s2){
-    if(char in counter){
-      counter[char] -= 1
-    }else{
-      return false;
-    }
+
+
+  for(let char of s2){
+    if(!(char in counter)){
+      return false
+    }else{
+      counter[char] -=1;
+    }
   }
-  
-  for(char in counter){
-    if(counter[char] !== 0){
-      return false;
-    }
+
+
+  for(let char in counter){
+    if(counter[char] !== 0) return false
   }
-  
-  return true
+
+
+  return true;
 };
-​
+
+
 module.exports = {
-  anagrams,
+  anagrams,
 };
-​
+
