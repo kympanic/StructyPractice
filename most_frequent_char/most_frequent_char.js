abe516c9632d24160cbd15b580965f76d22af93d
Daniel Yoo <dacapo0509@gmail.com>
2024-05-03 14:29:22 -0700

    Solved 'most frequent char' (.js)

--- most_frequent_char/most_frequent_char.js
+++ most_frequent_char/most_frequent_char.js
@@ -1,28 +1,27 @@
+mostFrequentChar('bookeeper'); // -> 'e'
+mostFrequentChar('david'); // -> 'd'
+mostFrequentChar('abby'); // -> 'b'
+mostFrequentChar('mississippi'); // -> 'i'
+mostFrequentChar('potato'); // -> 'o'
+mostFrequentChar('eleventennine'); // -> 'e'
+mostFrequentChar("riverbed"); // -> 'r'
 const mostFrequentChar = (s) => {
   let counter = {};
   for(let char of s){
     if(!(char in counter)){
-      counter[char] = 1;
-    }else{
-      counter[char] += 1;
+      counter[char] = 0;
     }
+    counter[char] += 1;
   }
 
 
   let best = null;
-  for(let char in counter){
-    if(best === null || counter[char] > counter[best]){
-      best = char;
-    }
+  for(let char of s){
+    if(best === null || counter[char] > counter[best]) best = char  
   }
 
 
-
-
-
-
   return best;
-  
 };
 
 
