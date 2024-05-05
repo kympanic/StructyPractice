93eb2bf5516a69cf72505d93b2738b66087dcbe6
Daniel Yoo <dacapo0509@gmail.com>
2024-05-04 22:51:50 -0700

    Solved 'pair sum' (.js)

--- pair_sum/pair_sum.js
+++ pair_sum/pair_sum.js
@@ -1,17 +1,21 @@
+pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
+pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
+pairSum([4, 7, 9, 2, 5, 1], 3); // -> [3, 5]
+pairSum([1, 6, 7, 2], 13); // -> [1, 2]
+pairSum([9, 9], 18); // -> [0, 1]
+pairSum([6, 4, 2, 8 ], 12); // -> [1, 3]
 const pairSum = (numbers, targetSum) => {
-  let previousNums = {}
-  
-  for(let i = 0; i < numbers.length; i++){
-    let number = numbers[i]
-    let complement = targetSum - number
-    if(complement in previousNums) return [previousNums[complement],i]
-    previousNums[number] = i;
+  let pairs = {};
+  for(let i = 0; i < numbers.length; i++){
+    let num = numbers[i]
+    let complement = targetSum - num;
+    if(complement in pairs) return [i,pairs[complement]]
+    pairs[num] = i
   }
-  
-  
 };
-​
+
+
 module.exports = {
-  pairSum,
+  pairSum,
 };
-​
+
