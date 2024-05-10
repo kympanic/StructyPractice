b1bca463f2f73d44842ca787565e4408ece17616
Daniel Yoo <dacapo0509@gmail.com>
2024-05-09 17:51:20 -0700

    Solved 'factorial' (.js)

---
+++ factorial/factorial.js
@@ -0,0 +1,20 @@
+For example, the factorial of 6 is:
+
+
+6 * 5 * 4 * 3 * 2 * 1 = 720
+factorial(3); // -> 6
+factorial(6); // -> 720
+factorial(18); // -> 6402373705728000
+factorial(1); // -> 1
+factorial(13); // -> 6227020800
+factorial(0); // -> 1
+const factorial = (n) => {
+  if(n === 0) return 1;
+  return n * factorial(n-1)
+};
+
+
+module.exports = {
+  factorial,
+};
+
