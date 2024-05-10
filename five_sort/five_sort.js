ec107b879e52cca88087f780ffad1aed678080de
Daniel Yoo <dacapo0509@gmail.com>
2024-05-09 17:46:42 -0700

    Solved 'five sort' (.js)

--- five_sort/five_sort.js
+++ five_sort/five_sort.js
@@ -1,19 +1,40 @@
+fiveSort([12, 5, 1, 5, 12, 7]);
+// -> [12, 7, 1, 12, 5, 5] 
+fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]);
+// -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5] 
+fiveSort([5, 5, 5, 1, 1, 1, 4]);
+// -> [4, 1, 1, 1, 5, 5, 5] 
+fiveSort([5, 5, 6, 5, 5, 5, 5]);
+// -> [6, 5, 5, 5, 5, 5, 5] 
+fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]);
+// -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5] 
+const fives = new Array(20000).fill(5);
+const fours = new Array(20000).fill(4);
+const nums = [...fives, ...fours];
+fiveSort(nums);
+// twenty-thousand 4s followed by twenty-thousand 5s
+// -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
 const fiveSort = (nums) => {
-  let i = 0;
-  let j = nums.length - 1;
-  while( i < j){
-    if(nums[j] === 5){
-      j--;
-    }else if(nums[i] === 5){
-      [nums[i],nums[j]] =[nums[j],nums[i]]
-    }else{
-      i++
-    }
+  let i = 0;
+  let j = nums.length - 1;
+
+
+  while(i < j){
+    if(nums[j] === 5){
+      j -= 1
+    }else if(nums[i] === 5){
+      [nums[i],nums[j]] =[nums[j], nums[i]]
+      i += 1
+    }else{
+      i += 1;
+    }
+    
   }
-  return nums;
+  return nums
 };
-​
+
+
 module.exports = {
-  fiveSort,
+  fiveSort,
 };
-​
+
