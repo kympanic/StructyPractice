f28939db49c85920394d7e097bb700423268a05b
Daniel Yoo <dacapo0509@gmail.com>
2024-05-13 16:11:57 -0700

    Solved 'sum list' (.js)

--- sum_list/README.txt
+++ sum_list/README.txt
@@ -8,12 +8,15 @@ const c = new Node(3);
 const d = new Node(-1);
 const e = new Node(7);
 
+
 a.next = b;
 b.next = c;
 c.next = d;
 d.next = e;
 
+
 // 2 -> 8 -> 3 -> -1 -> 7
+
 
 sumList(a); // 19
 
@@ -22,9 +25,12 @@ test_01
 const x = new Node(38);
 const y = new Node(4);
 
+
 x.next = y;
 
+
 // 38 -> 4
+
 
 sumList(x); // 42
 
@@ -32,7 +38,9 @@ test_02
 
 const z = new Node(100);
 
+
 // 100
+
 
 sumList(z); // 100
 
--- sum_list/sum_list.js
+++ sum_list/sum_list.js
@@ -1,17 +1,54 @@
+const a = new Node(2);
+const b = new Node(8);
+const c = new Node(3);
+const d = new Node(-1);
+const e = new Node(7);
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+
+// 2 -> 8 -> 3 -> -1 -> 7
+
+
+sumList(a); // 19
+const x = new Node(38);
+const y = new Node(4);
+
+
+x.next = y;
+
+
+// 38 -> 4
+
+
+sumList(x); // 42
+const z = new Node(100);
+
+
+// 100
+
+
+sumList(z); // 100
+sumList(null); // 0
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
 // }
-​
+
+
 const sumList = (head) => {
-  if(head === null) return 0;
-  if(head.next === null)return head.val
-  return head.val + sumList(head.next)
+  if(head === null) return 0;
+  return head.val + sumList(head.next)
 };
-​
+
+
 module.exports = {
-  sumList,
+  sumList,
 };
-​
+
