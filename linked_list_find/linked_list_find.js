9d8318f1305b5fe536c6dd645821caf970d9d9d9
Daniel Yoo <dacapo0509@gmail.com>
2024-05-13 16:51:25 -0700

    Solved 'linked list find' (.js)

--- linked_list_find/linked_list_find.js
+++ linked_list_find/linked_list_find.js
@@ -1,20 +1,90 @@
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+
+// a -> b -> c -> d
+
+
+linkedListFind(a, "c"); // true
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+
+// a -> b -> c -> d
+
+
+linkedListFind(a, "d"); // true
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+
+// a -> b -> c -> d
+
+
+linkedListFind(a, "q"); // false
+const node1 = new Node("jason");
+const node2 = new Node("leneli");
+
+
+node1.next = node2;
+
+
+// jason -> leneli
+
+
+linkedListFind(node1, "jason"); // true
+const node1 = new Node(42);
+
+
+// 42
+
+
+linkedListFind(node1, 42); // true
+const node1 = new Node(42);
+
+
+// 42
+
+
+linkedListFind(node1, 100); // false
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
 // }
-​
+
+
 const linkedListFind = (head, target) => {
-  let current = head;
-  while(current !== null){
-    if(current.val === target) return true;
-    current = current.next;
-  }
-  return false
+  if(head === null) return false;
+  if(head.val === target) return true;
+  return linkedListFind(head.next,target)
+  
 };
-​
+
+
 module.exports = {
-  linkedListFind,
+  linkedListFind,
 };
-​
+
